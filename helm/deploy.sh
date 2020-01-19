13ed33fa3ce9b34811ea5b292968985a6cedad32
Barbaros Alp <barbaros.alp@irdeto.com>
2020-01-19 20:23:56 +0100

    Dependency update

--- helm/deploy.sh
+++ helm/deploy.sh
@@ -25,19 +25,17 @@ eval $(minikube docker-env)
 docker build -t barb/vertx-api:1.0-SNAPSHOT .
 
 # Application Chart
-helm dependency update "${WORK_DIR}"/vertx-api
-helm package "${WORK_DIR}"/vertx-api --destination "${HELM_LOCAL_REPO}"
+helm package --dependency-update "${WORK_DIR}"/vertx-api --destination "${HELM_LOCAL_REPO}"
 
 # Add application chart to local helm repo
 helm repo index "${HELM_LOCAL_REPO}" --url http://127.0.0.1:8879/charts
 helm repo update
 
 # Umbrella Chart
-helm dependency update "${WORK_DIR}"/component
-helm package "${WORK_DIR}"/component --destination "${HELM_LOCAL_REPO}"
+helm package --dependency-update "${WORK_DIR}"/component --destination "${HELM_LOCAL_REPO}"
 
 # Install
-helm upgrade --install component  -f "${WORK_DIR}"/component-values.yaml "${HELM_LOCAL_REPO}"/component-1.0.0.tgz
+helm upgrade --install component -f "${WORK_DIR}"/component-values.yaml "${HELM_LOCAL_REPO}"/component-1.0.0.tgz
 
 # Wait for healthcheck
 kubectl rollout status --watch=true deployment component-vertx-api --timeout=60s
